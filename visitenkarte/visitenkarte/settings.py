5a0df68af2807b85185a360f1e237b33e5d5da5e
Madjita <xok-s@yandex.ru>
2018-05-08 15:35:12 +0700

    HTML page
    Make the empty html page

--- visitenkarte/visitenkarte/settings.py
+++ visitenkarte/visitenkarte/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'karte.apps.KarteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
